d9af10a10295b5ed70d1b87aace44016bf0092cc
BRYAN OLIVEIRA DE CARVALHO <bryan.carvalho@aluno.senac.rio>
2026-05-27 21:29:07 -0300

    27/05/2026 - Final

--- loja/views.py
+++ loja/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'loja/inicio.html')
+
+def produtos(request):
+    return render(request, 'loja/produtos.html')
